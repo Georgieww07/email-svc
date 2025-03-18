9e52891899f634b4cef3131983bf9a615ad0ba91
Georgi Georgiev <gogeorgievgo@gmail.com>
2025-03-18 15:31:45 +0200

    Initial commit

---
+++ src/test/java/com/emailsvc/EmailSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
