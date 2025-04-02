8fb87c57198788b3df00415c0a66cdf5459e177f
Georgi Georgiev <gogeorgievgo@gmail.com>
2025-04-03 00:00:42 +0300

    Following naming convention, renamed application class from EmailSvcApplication to Application

--- src/test/java/com/emailsvc/ApplicationTests.java
+++ src/test/java/com/emailsvc/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmailSvcApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
